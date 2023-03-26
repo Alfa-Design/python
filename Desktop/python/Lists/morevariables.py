cca4ae8b34c1ce7fe86decafc3924d1661453452
Alfa-Design <john7ouma@gmail.com>
2023-03-27 00:31:13 +0300

    variables and ata types

---
+++ Desktop/python/Lists/morevariables.py
@@ -0,0 +1,11 @@
+#Accesing the Address
+
+num = 10
+a = 10
+b = 8
+
+#print(num)
+#print(str(num))
+print(id(num)) #the adress of a and num will be simillar since they both point to the same value 10
+print(id(a)) #Python is memory efficient
+print(id(b)) #the address is different from the two because it oints to another value.
